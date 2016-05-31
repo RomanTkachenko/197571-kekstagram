ae7681a4afe4bad4433bb835dece57a3562c85ed
RomanTkachenko <iac2004@ukr.net>
2016-05-31 16:47:54 +0300

    Модуль 1 задание 2
    1. Использовал оператор switch для булевых значений переменной "а".
    2. Добавил "return" для всех мест, где возможно получение итогового
    результата.
    3. Использовал Array.isArray () для условия отбора  в случае массива.
    4. Добавил недостающие пробелы.

--- build/js/check.js
+++ build/js/check.js
@@ -1,28 +1,30 @@
 function  getMessage (a, b){
 var message;
-  if (typeof a=="boolean") {
-    if(a) {
-    message="Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-    }  else {
-    message="Переданное GIF-изображение не анимировано";
-    }
+switch (a) {
+  case true: return message="Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+  break;
+  case false: return message="Переданное GIF-изображение не анимировано";
+    console.log("2");
+  break;
+}
+
+if (typeof a=="number") {
+  return message="Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
+}
+
+if (Array.isArray(a) && Array.isArray(b)==false){
+  var sum=0;
+  for(i=0; i<a.length; i++){
+    sum=sum+a[i];
   }
-  else if (typeof a=="number") {
-    message="Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
+  return message="Количество красных точек во всех строчках изображения: "+sum;
+}
+
+if (Array.isArray(a) && Array.isArray(b)){
+  var square=0;
+  for(i=0; i<a.length; i++){
+    square=square+a[i]*b[i];
   }
-  else if (typeof a=="object"&&typeof b!=="object"){
-    var sum=0;
-    for(i=0; i<a.length;i++){
-      sum=sum+a[i];
-    }
-    message="Количество красных точек во всех строчках изображения: "+sum;
-  }
-  else  if (typeof a=="object"&&typeof b=="object"){
-    var square=0;
-    for(i=0; i<a.length; i++){
-      square=square+a[i]*b[i];
-    }
-    message="Общая площадь артефактов сжатия: "+square+" пикселей";
-  }
-return message;
+  return message="Общая площадь артефактов сжатия: "+square+" пикселей";
+}
 }
