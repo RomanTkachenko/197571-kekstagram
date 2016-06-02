4b07b0fafeeabb83de7d678fa021a475cee56ffa
RomanTkachenko <iac2004@ukr.net>
2016-06-02 14:49:11 +0300

    Модуль 1 задание2
    Поправлена отбивка. Удалена переменная message.

--- build/js/check.js
+++ build/js/check.js
@@ -1,30 +1,30 @@
 function  getMessage (a, b){
-var message;
 var typeOfA=typeof a;
 switch (typeOfA) {
   case "boolean":
     if(a) {
-      return message="Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
     } else {
-      return message="Переданное GIF-изображение не анимировано";
+      return "Переданное GIF-изображение не анимировано";
     }
   break;
-  case "number": return message="Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
+  case "number":
+    return "Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
   break;
   case "object":
     if (Array.isArray(a) && Array.isArray(b)==false){
       var sum=0;
       for(i=0; i<a.length; i++){
-      sum=sum+a[i];
+        sum=sum+a[i];
       }
-    return message="Количество красных точек во всех строчках изображения: "+sum;
+      return "Количество красных точек во всех строчках изображения: "+sum;
     }
     if (Array.isArray(a) && Array.isArray(b)){
       var square=0;
       for(i=0; i<a.length; i++){
-      square=square+a[i]*b[i];
+        square=square+a[i]*b[i];
       }
-    return message="Общая площадь артефактов сжатия: "+square+" пикселей";
+      return "Общая площадь артефактов сжатия: "+square+" пикселей";
     }
   break;
 }
