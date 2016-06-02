3832d6ca75cdd50a662e89005ef5b1f351d39b07
RomanTkachenko <iac2004@ukr.net>
2016-06-02 09:54:52 +0300

    Модуль 1 задание 2
    функция реализована с помощью оператора switch

--- build/js/check.js
+++ build/js/check.js
@@ -1,30 +1,31 @@
 function  getMessage (a, b){
 var message;
-switch (a) {
-  case true: return message="Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+var typeOfA=typeof a;
+switch (typeOfA) {
+  case "boolean":
+    if(a) {
+      return message="Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+    } else {
+      return message="Переданное GIF-изображение не анимировано";
+    }
   break;
-  case false: return message="Переданное GIF-изображение не анимировано";
-    console.log("2");
+  case "number": return message="Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
   break;
-}
-
-if (typeof a=="number") {
-  return message="Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
-}
-
-if (Array.isArray(a) && Array.isArray(b)==false){
-  var sum=0;
-  for(i=0; i<a.length; i++){
-    sum=sum+a[i];
-  }
-  return message="Количество красных точек во всех строчках изображения: "+sum;
-}
-
-if (Array.isArray(a) && Array.isArray(b)){
-  var square=0;
-  for(i=0; i<a.length; i++){
-    square=square+a[i]*b[i];
-  }
-  return message="Общая площадь артефактов сжатия: "+square+" пикселей";
+  case "object":
+    if (Array.isArray(a) && Array.isArray(b)==false){
+      var sum=0;
+      for(i=0; i<a.length; i++){
+      sum=sum+a[i];
+      }
+    return message="Количество красных точек во всех строчках изображения: "+sum;
+    }
+    if (Array.isArray(a) && Array.isArray(b)){
+      var square=0;
+      for(i=0; i<a.length; i++){
+      square=square+a[i]*b[i];
+      }
+    return message="Общая площадь артефактов сжатия: "+square+" пикселей";
+    }
+  break;
 }
 }
