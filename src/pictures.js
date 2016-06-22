0309f197130e42dcc5c5acb4d1b9bfe2f25e29e1
RomanTkachenko <iac2004@ukr.net>
2016-06-22 11:56:37 +0300

    Вставка данных полученных с сервера на страницу
    Undefined window.pictures....Что с этим делать?

--- src/pictures.js
+++ src/pictures.js
@@ -1,5 +1,44 @@
 'use strict';
 
+
+
 // Прячем блок с фильтрами
 var filterBlock = document.getElementsByClassName('filters');
 filterBlock[0].classList.add('hidden');
+
+var picturesContainer = document.querySelector('.pictures');
+console.log(picturesContainer);
+
+var templateElement = document.querySelector('template');
+console.log(templateElement);
+console.log(window.pictures);
+
+var elementToClone;
+
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.picture');
+} else {
+  elementToClone = templateElement.querySelector('.picture');
+}
+
+var getPictureElement = function(data, container) {
+  var element = elementToClone.cloneNode(true);
+  element.querySelector('.picture-comments').textContent = data.comments;
+  element.querySelector('.picture-likes').textContent = data.likes;
+
+
+  var galleryImage = new Image();
+  galleryImage.onload = function(evt) {
+    element.querySelector('img').url = '\'' + evt.target.src + '\'';
+  };
+  galleryImage.src = data.url;
+
+  container.appendChild(element);
+
+
+  return element;
+};
+
+window.pictures.forEach(function(picture) {
+  getPictureElement(picture, picturesContainer);
+});
